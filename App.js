be3ba1bd6585214c51d2c022c601d811bdb92f60
Naveen Kumar Reddy Murthi <>
2023-10-21 16:51:01 +0800

    Laying the foundation for food fire app

--- App.js
+++ App.js
@@ -1,16 +1,2 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "parent" }, [
-    React.createElement("h1", {}, "h1 tag"),
-    React.createElement("h2", {}, "h1 tag"),
-  ]),
-  React.createElement("div", { id: "parent" }, [
-    React.createElement("h1", {}, "h1 tag"),
-    React.createElement("h2", {}, "h1 tag"),
-  ]),
-]);
-
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
