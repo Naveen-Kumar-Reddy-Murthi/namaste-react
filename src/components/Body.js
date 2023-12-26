f4ef56cd73a17a000ccaa74e6fc7134a82d78112
Naveen Kumar Reddy Murthi <mnreddy.nv@gmail.com>
2023-12-26 19:57:29 +0800

    Add filter with name functionality and fix swiggy api issue to handle in the morning and evening

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,8 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   
   const [restaurants, setRestaurants] = useState([]);
+  const [filterRestaurants, setFilterRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(()=> {
     fetchData();
@@ -11,18 +13,35 @@ const Body = () => {
 
   const fetchData = async () =>{
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING")
-    const json = await data.json();
-    console.log(json);
+    // const json = await data.json();
+    // console.log("body rendered..");
+    //since swiggy api response data changes in the morning and evening, below code to handle in all times
+    const jsonData = await data.json();
+const restaurant_list = "restaurant_grid_listing";
+const restaurants = jsonData?.data?.cards.find(
+  (card) => card.card.card.id === restaurant_list 
+);
     setRestaurants(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      restaurants?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilterRestaurants(
+        restaurants?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
 
     return restaurants.length === 0 ? <Shimmer/> :(
       <div className="body">
         <div className="filter">
+          <div className="search">
+            <input type="text" className="search-box" value = {searchText}  onChange={(e) => {
+              setSearchText(e.target.value);
+            }} />
+            <button onClick={() =>{
+              const filteredRestaurants = filterRestaurants.filter((restaurant) => restaurant.info.name.toLowerCase().includes(searchText.toLowerCase()));
+              setRestaurants(filteredRestaurants);
+            }}>Search</button>
+          </div>
         <button className="filter-btn"
         onClick={()=> {
-           const topRatedRestaurants = restaurants.filter((restaurant) => restaurant.info.avgRating > 4.4);
+           const topRatedRestaurants = filterRestaurants.filter((restaurant) => restaurant.info.avgRating > 4);
             setRestaurants(topRatedRestaurants);
         }}
         >Top Rated</button>
