6a462e41ce7baf68db571e7c84ec67ec2ae3d3df
Naveen Kumar Reddy Murthi <mnreddy.nv@gmail.com>
2023-12-28 19:53:20 +0800

    use Link component for hyperlink on header options

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../utils/Constant";
 import { useState,useEffect } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
 
    const [buttonName, setButtonName] = useState("Login") 
@@ -16,9 +17,9 @@ const Header = () => {
         </div>
         <div className="nav-items">
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
             <li>Cart</li>
             <button className="login" onClick={() => {
               setButtonName(buttonName==="Logout"? "Login": "Logout")
