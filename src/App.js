90fc1b302d37d7819cbe6338e041c0d248453151
Naveen Kumar Reddy Murthi <mnreddy.nv@gmail.com>
2023-12-28 19:12:40 +0800

    Make header common using react router dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -28,7 +28,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -38,14 +38,20 @@ const appRouter = createBrowserRouter([
     path : "/",
     element: <AppLayout/>,
     errorElement: <Error/>,
-  },
-  {
-    path : "/about",
-    element: <About/>
-  },
-  {
-    path : "/contact",
-    element: <Contact/>
+    children : [
+      {
+        path : "/",
+        element: <Body/>
+      },
+      {
+        path : "/about",
+        element: <About/>
+      },
+      {
+        path : "/contact",
+        element: <Contact/>
+      },
+    ]
   },
 ])
 
